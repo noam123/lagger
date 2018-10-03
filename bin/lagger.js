acfc0daa28cef9c3f247acaabc0eeac105fe3592
noamdoron <noam.doron@microfocus.com>
2018-10-03 08:17:23 +0300

    init

---
+++ bin/lagger.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+var spawn = require('child_process').spawn,
+    path = require('path'),
+    fs = require('fs'),
+    args = [path.join(__dirname, '../app.js')];
+
+process.argv.slice(2).forEach(function(arg){
+    var flag = arg.split('=')[0];
+
+    switch (flag) {
+        case '-f':
+        case '-v':
+            args.push(arg);
+        default:
+            break;
+    }
+});
+
+// console.log(`spawn ${process.execPath}, args: ${args}`)
+var proc = spawn(process.execPath, args, { stdio: 'inherit' });
+proc.on('exit', function (code, signal) {
+    process.on('exit', function(){
+        if (signal) {
+            process.kill(process.pid, signal);
+        } else {
+            process.exit(code);
+        }
+    });
+});
+
+// terminate children.
+process.on('SIGINT', function () {
+    proc.kill('SIGINT'); // calls runner.abort()
+    proc.kill('SIGTERM'); // if that didn't work, we're probably in an infinite loop, so make it die.
+});
---
+++ readme.md
@@ -0,0 +1,17 @@
+
+***Installation***
+<br> npm install . -g
+<br>
+***Supported Consoles***
+<br> - Git bash 
+<br>
+***Notes***
+<br>for git bash color support use environment variable FORCE_COLOR with any value to force color
+<br>
+<br>
+***RELEASE NOTES***
+ <br><br>
+ <b>1.0.3</b><br>
+ - FIx new line breaks log
+
+
