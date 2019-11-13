fcbe31a195c8933ccace37ff8d0108bacb8c6fd1
noamdoron <noam@codefresh.com>
2019-11-13 08:20:43 +0200

    [Cosmetics] removed redundant help function

--- lib/app.ts
+++ lib/app.ts
@@ -60,28 +60,3 @@ if (!outputChannel) {
 
 const formatter: Formatter = FormatterFactory.create(format);
 outputChannel.output(formatter);
-
-function helpString() {
-    return `
-    Lagger (${packageJson.version})
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    Command execution:
-    lagger [OPTIONS]
-    
-    OPTIONS:
-    [-f]    
-            Path to the file which needs to be formatted.
-            i.e. lagger -f=/tmp/file.log 
-    
-    [--format]
-            Formatter type (defualt: cf)
-        
-    [--performance-threshold, -p]    
-            Performance millisecond gap threshold, if and only if threshold has been hit, log lines will be printted with the time it took (in ms) 
-            i.e. lagger -performance-threshold=50000
-            
-    [-v]    
-            Show lagger version.
-            i.e. lagger -v
-    `
-}
