fb4fa0df144e0f255f0137a3db3ad1c31d04813f
noamdoro <noamdoro@mobileye.com>
2023-02-22 14:35:55 +0200

    [US] added dicker.sh

---
+++ bin/dicker.sh
@@ -0,0 +1,43 @@
+
+#echo $@
+rest_of_args=${@: 3}
+echo $2 $rest_of_args
+
+function pause(){
+   read -p "$*"
+}
+
+case "$1" in
+        start)
+            docker start $(docker ps -a | grep $2 | awk '{print $1;}' | xargs )
+            ;;
+         
+        stop)
+             docker stop $(docker ps | grep $2 | awk '{print $1;}' | xargs )
+            ;;
+         
+        logs)
+            docker logs -f $(docker ps | grep $2 | awk '{print $1;}' | xargs ) $rest_of_args
+            ;;
+        
+		rm)
+            docker rm -f $(docker ps | grep $2 | awk '{print $1;}' | xargs )
+            ;; 
+        restart)
+            docker restart $(docker ps | grep $2 | awk '{print $1;}' | xargs )
+            ;; 
+        exec)
+            winpty docker exec -it $(docker ps | grep $2 | awk '{print $1;}' | xargs ) $3
+            ;;
+        inspect)
+            docker inspect $(docker ps | grep $2 | awk '{print $1;}' | xargs )
+            ;;                         			
+        *)
+            echo $"Usage: $0 {start|stop|logs|rm}"
+            exit 1
+ 
+esac
+
+
+
+
